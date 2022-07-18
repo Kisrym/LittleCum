8bb9c48659461b6421be211d15c97fbd2ad24b72
Kaio Santos Araújo <kaiosantos568@gmail.com>
2022-07-18 19:49:50 -0300

    Adicionando funções da Database online e atualizando mensagens

---
+++ functions/database.py
@@ -0,0 +1,23 @@
+import requests, os
+from dotenv import load_dotenv
+from func import json_read, json_dump
+
+load_dotenv()
+database = os.getenv("DATABASE")
+
+def exportar_database():
+    data = {}
+
+    data['config'] = json_read(r"C:\Users\kaios\Desktop\Storage\Programação\projetos\LittleCum\db\config.json")
+    data['commands'] = json_read(r"C:\Users\kaios\Desktop\Storage\Programação\projetos\LittleCum\db\commands.json")
+    data['moneycum'] = json_read(r"C:\Users\kaios\Desktop\Storage\Programação\projetos\LittleCum\db\moneycum.json")
+
+    data = str(data).replace("\'", "\"")
+
+    requests.patch(database, data = f'{data}')
+
+def importar_database():
+    r = requests.get(database).json()
+    json_dump(r"db\config.json", r['config'])
+    json_dump(r"db\commands.json", r['commands'])
+    json_dump(r"db\moneycum.json", r['moneycum'])
